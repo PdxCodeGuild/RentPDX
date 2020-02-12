d6718cfb8b78a9d40dd59952d8e74e9b21abd438
WileyRummel <wiley.rummel@gmail.com>
2020-02-12 15:27:20 -0800

    Started Vue Components
    -Login
    -Dashboard
    -Favorites

---
+++ frontend/rentpdx/src/components/favorites.vue
@@ -0,0 +1,26 @@
+<template>
+    <h1>
+        Hello from favorites
+    </h1>
+</template>
+
+<script>
+export default {
+    data() {
+
+    },
+
+    methods: {
+
+    },
+    
+    computed: {
+
+    },
+
+}
+</script>
+
+<style scoped>
+
+</style>
